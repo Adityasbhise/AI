b50f1cd78229cde218ecbdf15271ac6acd7afb4b
CosmicKtulu <114893340+CosmicKtulu@users.noreply.github.com>
2024-01-19 10:52:03 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,51 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def dfs(self, start, visited=None):
+        if visited is None:
+            visited = set()
+        visited.add(start)
+        print(start, end=' ')
+
+        for neighbor in self.graph[start]:
+            if neighbor not in visited:
+                self.dfs(neighbor, visited)
+
+    def bfs(self, start):
+        visited = set()
+        queue = [start]
+        visited.add(start)
+
+        while queue:
+            current = queue.pop(0)
+            print(current, end=' ')
+
+            for neighbor in self.graph[current]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("DFS starting from vertex 2:")
+g.dfs(2)
+
+print("\nBFS starting from vertex 2:")
+g.bfs(2)
+
